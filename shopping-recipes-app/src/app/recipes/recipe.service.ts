ef526f0fdaadaa7ebfdf306d01a52574cacd0b01
Harish Kavatekar <h.kavatekar@gmail.com>
2018-09-29 13:56:45 +0530

    added services to the shopping and recipe

---
+++ shopping-recipes-app/src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  receipeSelcted = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService) { }
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'test',
+      'this is the test',
+       'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2018%2F02%2Fmain%2F2548601_qfsba_rice_with_scallops_152.jpg%3Fitok%3Dqvkz_lUq&w=800&q=85',
+        [
+          new Ingredient('bread', 2),
+          new Ingredient('cake', 3)
+        ]),
+    new Recipe(
+      'Another test',
+      'this is the test', 
+      'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2018%2F02%2Fmain%2F2548601_qfsba_rice_with_scallops_152.jpg%3Fitok%3Dqvkz_lUq&w=800&q=85',
+      [
+        new Ingredient('bread', 2),
+        new Ingredient('cake', 3)
+      ])
+    ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  onAddIngredientToShoppingList(ingredients: Ingredient[]) {
+    console.log('recipe service');
+    this.slService.addIngredients(ingredients);
+  }
+}
---
+++ shopping-recipes-app/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('apple',5),
+    new Ingredient('orange',5)
+  ];
+
+  constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+  console.log('shopping list service');
+   this.ingredients.push(...ingredients);
+   this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
